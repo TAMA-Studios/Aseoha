e21847e45848baf9ce21839537336ea5bf671bc0
TheDJGamerYT <156543381+TheDJGamerYT@users.noreply.github.com>
2026-03-08 14:38:12 -0500

    The AI had notes on the humans code. oh boy
    Added logic to handle client-side container closure and send EnterRWFPacket.

--- src/main/java/com/code/aseoha/protocol/RWFProtocol.java
+++ src/main/java/com/code/aseoha/protocol/RWFProtocol.java
@@ -1,9 +1,9 @@
 package com.code.aseoha.protocol;
 
 import com.code.aseoha.Helpers.IHelpWithConsole;
+import com.code.aseoha.networking.Packets.EnterRWFPacket;
+import com.code.aseoha.aseoha;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.tardis.mod.constants.TardisConstants;
 import net.tardis.mod.protocols.Protocol;
@@ -12,10 +12,22 @@ import net.tardis.mod.tileentities.ConsoleTile;
 public class RWFProtocol extends Protocol {
     @Override
     public void call(World world, PlayerEntity playerIn, ConsoleTile console) {
-        if (world.isClientSide)
+        // closeContainer must run client-side; all logic below is server-side only
+        if (world.isClientSide) {
             playerIn.closeContainer();
+            return;
+        }
+
         console.setPilot(playerIn);
         ((IHelpWithConsole) console).Aseoha$SetRealWorldFlight(true);
+
+        // Send the packet that actually kicks off entity spawning and teleportation.
+        // Without this the RWF flag was set but nothing ever happened.
+        aseoha.NETWORK.send(
+                net.minecraftforge.fml.network.PacketDistributor.PLAYER.with(
+                        () -> (net.minecraft.entity.player.ServerPlayerEntity) playerIn),
+                new EnterRWFPacket(console.getLevel().dimension().location())
+        );
     }
 
     @Override
