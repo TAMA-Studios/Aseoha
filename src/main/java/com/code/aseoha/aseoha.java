997dcb7c4462aa4fa092d13bfe97bb8036de0a6f
TheDJGamerYT <156543381+TheDJGamerYT@users.noreply.github.com>
2026-03-08 19:06:22 -0500

    Remove mixin configuration if TAdditions is not loaded
    Remove mixin configuration for TAdditions mod.

--- src/main/java/com/code/aseoha/aseoha.java
+++ src/main/java/com/code/aseoha/aseoha.java
@@ -106,7 +106,6 @@ public class aseoha {
 
         if (!(ModList.get().isLoaded("tadditions"))) {
             NoTadditionsItems.register(modBus);
-            Mixins.addConfiguration("aseoha.tacompat.mixins.json");
         }
 
         if (!(ModList.get().isLoaded("project_tardis") || ModList.get().isLoaded("sidrat")))
